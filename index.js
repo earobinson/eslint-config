bef0b0140fba5f44532dc5beeb7f3112781847b4
Edward Andrew Robinson <earobinson@gmail.com>
2019-02-06 13:30:48 -0500

    removed comments

--- index.js
+++ index.js
@@ -5,29 +5,6 @@ module.exports = {
   'rules': {
     'linebreak-style': ['error', 'unix'],
     'semi': ['error', 'always'],
-    //   'space-before-function-paren': ['error', {
-    //     'anonymous': 'never',
-    //     'named': 'never'
-    //   }],
-    //   'no-multiple-empty-lines': ['error', {
-    //     'max': 2,
-    //     'maxEOF': 0
-    //   }],
-    'no-mixed-spaces-and-tabs': 'error',
-    'no-trailing-spaces': 'error',
-    //   'no-spaced-func': 'error',
-    'quotes': ['error', 'single', {
-      'avoidEscape': true
-    }],
-    'comma-spacing': ['error', {
-      'before': false, 'after': true
-    }],
-    'comma-style': ['error', 'last']
-  //   'space-before-blocks': ['error'],
-  //   'arrow-spacing': ['error'],
-  //   'object-curly-spacing': ['error', 'always'],
-  //   'space-in-parens': ['error', 'never'],
-  //   'object-shorthand': ['error'],
-  //   'arrow-parens': ['error', 'always']
+    'no-console': ['error']
   }
 };
